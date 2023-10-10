f3ac3528dd6250b75613e88cd0cf381f151636a8
Firman Dzaki R <144086824+Frmndz@users.noreply.github.com>
2023-10-10 15:11:44 +0700

    Add files via upload

---
+++ DoWhileCuti13_1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class DoWhileCuti13_1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int jumlahHari, jatahCuti;
+        String konfirmasi;
+
+        System.out.println("Jatah cuti : ");
+        jatahCuti = sc.nextInt();
+
+        do{
+            System.out.println("Apakah anda ingin mengambil cuti ? (y/t)");
+            konfirmasi = sc.next();
+
+            if (konfirmasi.equalsIgnoreCase("y")) {
+                System.out.println("Masukkan jumlah hari cuti anda : ");
+                jumlahHari = sc.nextInt();
+
+                if (jumlahHari <= jatahCuti ){
+                    jatahCuti -= jumlahHari;
+                    System.out.println("Sisa jumlah cuti : " + jatahCuti);
+                } else if ( jatahCuti == 0){
+                    break;
+                } else {
+                    System.out.println("Jatah cuti tidak tersedia, sisa jatah cuti anda tinggal " + jatahCuti);   
+                }     
+            }
+        } while (jatahCuti > 0);
+}
+    }
+
