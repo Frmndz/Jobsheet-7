2cb7cf56df8a8f21367992f1482cbfd4ef19de50
Firman Dzaki R <144086824+Frmndz@users.noreply.github.com>
2023-10-10 15:57:21 +0700

    Add files via upload

---
+++ DoWhile13.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class DoWhile13 {
+    public static void main(String[] args){
+        Scanner sc16 =new Scanner(System.in);
+
+        int x = 0;
+        do {
+            System.out.println(x);
+        } while (++x <= 8);
+        System.out.println("Program berhenti");
+
+        // int x = 10;
+        // do {
+        //    System.out.println(x);
+        // } while (++x <= 8);
+        // System.out.prrintln("Program berhenti");
+    }
+}
